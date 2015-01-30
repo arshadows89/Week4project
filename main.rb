6b16ad24982644cae65b1c86d8324f491ac749ea
Cameron Snyder <Dooffycom@gmail.com>
2015-01-30 18:17:51 -0500

    added follower and following database

--- main.rb
+++ main.rb
@@ -121,6 +121,16 @@ post '/post_feed' do
   redirect '/profile'
 end
 
+get '/follow/:id' do
+  @relationship = Relationship.new(follower_id: current_user.id, followed_id: params[:id])
+  if @relationship.save
+    flash[:notice] = "You've successfully followed #{User.find(params[:id]).fname}."
+  else
+    flash[:alert] = "There was an error following that user."
+  end
+  redirect back
+end
+
 
 
 
