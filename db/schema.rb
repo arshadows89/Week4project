ef23e3a9339c959afc5e70f3c4a4849c62a8b157
Master <subtle3ntropy@gmail.com>
2015-01-31 14:55:01 -0500

    File name changed

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150130213006) do
+ActiveRecord::Schema.define(version: 20150130230639) do
 
   create_table "posts", force: :cascade do |t|
     t.string  "title"
     t.text    "content"
     t.integer "user_id"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
   end
 
   create_table "users", force: :cascade do |t|
